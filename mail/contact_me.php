5927960db2d1926c1298b00891dc37931a24ff21
Zakaria EL BAZI <za.elbazi@insea.ac.ma>
2019-10-01 22:20:23 +0200

    Update contact_me.php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,7 +16,7 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
-$to = 'contact@inseait.com';
+$to = 'zakaria@elbazi.me';
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: noreply@inseait.com\n";
